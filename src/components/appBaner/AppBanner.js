62fb087d219145d4d94ade78124f4faebfae49b5
Vlad Halaiko <lorddarkvlad@gmail.com>
2023-03-11 01:40:39 +0200

    add media rules

--- src/components/appBaner/AppBanner.js
+++ src/components/appBaner/AppBanner.js
@@ -1,10 +1,14 @@
-import banner from './rickandmorty.png';
+import bannerDesktop from './rickandmorty.png';
+import bannerMobile from './rickandmortymobile.png';
 import './appBanner.scss'
+
 const AppBanner = () => {
     return (
         <div className="app__banner">
-            <img src={banner} alt="banner" />
+            <img className="app__baner_img" src={bannerDesktop} alt="banner" />
+            <img className="app__baner_img--mobile" src={bannerMobile} alt="banner" />
         </div>
     )
 }
+
 export default AppBanner;
